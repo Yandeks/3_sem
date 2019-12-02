ce4570e72263e350dbb49adf1ecb0bde69db1877
Yandeks <47361238+Yandeks@users.noreply.github.com>
2019-12-02 16:37:19 +0300

    task2 done

--- dz2/main2.c
+++ dz2/main2.c
@@ -1,143 +1,87 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 #include <sys/wait.h>
 
-typedef struct{
-	int ffd[2];
-	int bfd[2];
+#define size_ 128
+
+typedef struct {
+	int txd[2];
+	int rxd[2];
 } fdpipe_t;
 
-#define length 13
-
-void shiftbuf (int fd, char* buf, int size)
-{
-	char* shbuf = (char*)malloc((size+1)*sizeof(char));
-
-	shbuf[size-1] = buf[1];
-	for(int i = 0; i < size - 1; i++)
-	{
-		shbuf[i] = buf[i+1];
+char* shift (char* buf, size_t size ) {
+	char* temp2 = (char*)malloc((size+1)*sizeof(char));
+	int i;
+	temp2[0] = buf[size-2];
+	for (i =1; i < size-1; i++) {
+		temp2[i] = buf[i-1];
 	}
-
-	// to the execution 
-
-	printf("RECIEVED_FROM_PARENT: %s\n", buf);
-
-	write(fd, shbuf, size);
-
-	free(shbuf);
-
-	return;
+	temp2[size-1]='\0';
+	return temp2;
 }
 
-int main()
-{
-	char buf1[length];
-	char buf2[length];
 
+
+
+int main (){
+	char buf1[size_];
+	char buf2[size_];
 	int size1;
 	int size2;
-
 	fdpipe_t fd;
 	int status;
 
-	if(pipe(fd.ffd) < 0)
-	{
-		perror("Pipe_Creation_Failed");
-		return -1;
+	if(pipe(fd.txd) < 0 || pipe(fd.rxd) < 0) {
+		perror("pipe creation is failed");
+		exit(1);
 	}
 
-	if(pipe(fd.bfd) < 0)
-    {
-    	perror("Pipe_Creation_Failed");
-        return -1;
-    }
+	const pid_t pid = fork();
 
-    const pid_t pid = fork();
-    if(pid < 0)
-    {
-    	perror("Fork_Creation_Failed");
-		return -1;
-    }
+	if(pid < 0) {
+		perror("fork creation is failed");
+		exit(1);
+	}
 
-    if (pid)
-    {
-    	close(fd.ffd[1]);
-    	close(fd.bfd[0]);
+	if (pid > 0) {
+		close(fd.txd[0]);
+		close(fd.rxd[1]);
 
-    	while(1)
-    	{
-    		while((size1 = read(0, buf1, sizeof(buf1) - 1 )) > 1)
-    		{
-    			if(buf1[size1 - 1] == '\n')
-				size1--;
-				buf1[size1] = '\0';
-
-				write(fd.bfd[1], buf1, size1);
-
-				if(!strcmp(buf1,"exit"))
-                {
+		while(1) {
+			int size = read(0, buf1, sizeof(buf1) - 1);
+				buf1[size] = '\0';
+				write(fd.txd[1], buf1, size+1);
+				if(strcmp(buf1, "exit\n")==0) {
 					waitpid(pid, &status, 0);
-					printf("EXIT_CODE: %d\n", WEXITSTATUS(status));
-					return 0;
-                }
+					printf("process exited with %d\n", WEXITSTATUS(status));
+					exit(0);
+				}
+				printf("sent to child: %s\n", buf1);
 
-                printf("SEND_TO_CHILD: %s\n", buf1);
-
-                if(size1 < length - 1)
-                	break;
-
-    		}
-
-    		write(fd.bfd[1], "\n" , 1);
-
-    		while(1)
-    		{
-    			size2 = read(fd.ffd[0], buf2, sizeof(buf2) - 1);
-    			buf2[size2] = '\0';
-    			if(buf2[size2 - 1] == '\n')
-                {
-                	if(size2 == 1)
-                    	break;
-
-                    buf2[size2 - 1] = '\0';
-                    printf("RECIEVED_BACK_FROM_CHILD: %s\n", buf2);
-                    break;
-                }
-
-                printf("RECIEVED_BACK_FROM_CHILD: %s\n", buf2);
-    		}
-    	}
-    }
-    else
-    {
-    	close(fd.bfd[1]);
-    	close(fd.ffd[0]);
-
-    	while(1)
-    	{
-    		size1 = read(fd.bfd[0], buf1, sizeof(buf1) - 1);
-			buf1[size1] = '\0';
-			if(!strcmp(buf1,"exit"))
-				exit(1);
-
-			if(buf1[size1 - 1] == '\n')
-			{
-				if(size1 == 1)
-					break;
-				buf1[size1 - 1] = '\0';
-				shiftbuf(fd.ffd[1], buf1, size1 - 1);
+			while(1) {
+				size2 = read(fd.rxd[0], buf2, sizeof(buf2) - 1);
+				printf("recieved from child: %s\n", buf2);
 				break;
 			}
+		}
+	} else {
+		close(fd.rxd[0]);
+		close(fd.txd[1]);
 
-			shiftbuf(fd.ffd[1], buf1, size1);
-    	}
-
-			write(fd.ffd[1], "\n", 1);
-    }
-
-return 0;
+		while(1) {
+			while((size1 = read(fd.txd[0], buf1, sizeof(buf1) - 1)) == 0);
+			if(strcmp(buf1, "exit\n")==0)
+			{	
+				exit(0);
+			}
+			printf("recieved from parent: %s\n", buf1);
+			char* temp = shift(buf1, size1-1);
+			printf("sent to parent: %s\n", temp);
+			write(fd.rxd[1], temp, size1);
+			free(temp);
+			
+		}
+	}
 }
